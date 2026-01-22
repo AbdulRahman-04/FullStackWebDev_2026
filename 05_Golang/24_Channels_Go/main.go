06beea1eebf6826aa79a9f1a49db29ba39853b00
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-01-23 03:13:01 +0530

    Channels in Go

--- 05_Golang/24_Channels_Go/main.go
+++ 05_Golang/24_Channels_Go/main.go
@@ -1,3 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func worker1(wg *sync.WaitGroup, myChan chan int) {
+// 	defer wg.Done()
+
+// 	// sender ready
+// 	myChan <- 160321735049
+// }
+
+// func main() {
+
+// 	fmt.Println("main go routine started✅")
+
+// 	var wg sync.WaitGroup
+
+// 	// unbuffered channel: sender and receiver must be ready at the same time,
+// 	// otherwise the goroutine will block and may cause a deadlock
+
+// 	// creating unbuffered channel
+// 	myChan := make(chan int)
+
+// 	wg.Add(1)
+
+// 	go worker1(&wg, myChan)
+
+// 	// reciever ready
+// 	msg := <-myChan
+// 	fmt.Println(msg)
+
+// 	wg.Wait()
+
+// }
+
 package main
 
 import (
@@ -5,30 +43,41 @@ import (
 	"sync"
 )
 
-func worker(wg *sync.WaitGroup, ch chan string){
-	defer wg.Done()
-
-	// sender ready
-	ch <- "ye le bhai worker go routine ka data"
-
+func worker2(wg*sync.WaitGroup, buffChan chan int){
+ defer wg.Done()
+ buffChan <- 49
+ buffChan <- 46
+ buffChan <- 42
+ buffChan <- 43
+ buffChan <- 58
+ buffChan <- 4044
+ buffChan <- 4048
+ buffChan <- 1604
+ buffChan <- 1603
+ buffChan <- 1602
 }
 
 func main(){
+	fmt.Println("main go routine started✅")
  
 	var wg sync.WaitGroup
 
-	// create a chan
-	ch := make(chan string)
-
 	wg.Add(1)
 
-	go worker(&wg, ch)
+// 	A buffered channel lets you send a fixed number of values without waiting for a receiver.
+// Sending blocks only when the buffer is full, and receiving blocks only when the buffer is empty.
 
-	// reciever ready
-    chanMsg := <- ch
+	// create buffered channel
+	buffChan := make(chan int, 5)
 
-	fmt.Println(chanMsg)
+	go worker2(&wg, buffChan)
+
+	// reciver
+	fmt.Println(<-buffChan, <-buffChan, <- buffChan, <- buffChan, <- buffChan)
+
+
+
+
 
 	wg.Wait()
-
 }
