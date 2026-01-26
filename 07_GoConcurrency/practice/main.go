44b1d260d9813eabdebe144f7c32d16c3a0df82b
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-01-27 00:24:29 +0530

    Go Concurrency Pattern

--- 07_GoConcurrency/practice/main.go
+++ 07_GoConcurrency/practice/main.go
@@ -44,52 +44,58 @@ import (
 	"time"
 )
 
-func worker(jobs chan string, wg *sync.WaitGroup, mainChann chan string) {
+func worker(jobs chan string, wg *sync.WaitGroup, mainChan chan string) {
+
 	defer wg.Done()
 	for url := range jobs {
-		time.Sleep(1 * time.Second)
-		fmt.Printf("url processed %s\n", url)
-
-		mainChann <- url
+		time.Sleep(1*time.Second)
+		mainChan <-url
 	}
+
 }
 
 func main() {
 
 	var wg sync.WaitGroup
-
+	// channel to recive data from worker thread
+	mainChan := make(chan string, 10)
+	// create jobs channel
 	jobs := make(chan string)
-	mainChann := make(chan string, 5)
 
-	// go worker("image1.png", &wg, mainChann)
-	// go worker("image2.png", &wg, mainChann)
-	// go worker("image3.png", &wg, mainChann)
-
-	// SLICE of jobs
 	images := []string{
-		"image1.png",
-		"image2.png",
-		"image3.png",
-		"image4.png",
-		"image5.png",
-	}
+	"image1.png",
+	"image2.png",
+	"image3.png",
+	"image4.png",
+	"image5.png",
+	"image6.png",
+	"image7.png",
+	"image8.png",
+	"image9.png",
+	"image10.png",
+}
+
 
 	wg.Add(3)
-	go worker(jobs, &wg, mainChann)
-	go worker(jobs, &wg, mainChann)
-	go worker(jobs, &wg, mainChann)
-
+	go worker(jobs, &wg, mainChan)
+	go worker(jobs, &wg, mainChan)
+	go worker(jobs, &wg, mainChan)
+ 
+	// slice k andar data -> jobs channel
 	for _, img := range images {
 		jobs <- img
 	}
+
 	close(jobs)
 
+	go func ()  {
 	wg.Wait()
-	close(mainChann)
-
-	for date := range mainChann {
-		fmt.Println(date)
+	close(mainChan)	
+	}()
+ 
+	for data := range mainChan {
+      fmt.Println("channel data is:", data)
 	}
-	fmt.Println("main go routine executed✅")
 
+	fmt.Println("main ggo routine executed✅")
 }
