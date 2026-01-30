c35e81e62d0c4e9f5dc6d98a9cd1e481fcd6b921
AbdulRahman-04 <abdulrahman.81869@gmail.com>
2026-01-30 08:00:37 +0530

    email sending logic

---
+++ 08_Go_Gin_PostgreSQL/server/internal/utils/sendEmail.go
@@ -0,0 +1,80 @@
+// package utils
+
+// import (
+// 	"fmt"
+
+// 	"gopkg.in/gomail.v2"
+// 	"github.com/AbdulRahman-04/FullStackWebDev_2026/08_Go_Gin_PostgreSQL/server/internal/config"
+// )
+
+// type EmailData struct {
+// 	To string
+// 	From string
+// 	Html string
+// 	Subject string
+// 	Text string
+// }
+
+// func SendEmail(data EmailData) error {
+// 	user := config.AppConfig.Email
+// 	pass := config.AppConfig.Pass
+
+// 	s := gomail.NewMessage()
+// 	s.SetAddressHeader("From", user, "Team Insta")
+// 	s.SetHeader("To", data.To)
+// 	s.SetHeader("Subject", data.Subject)
+// 	s.SetBody("Text/plain", data.Text)
+// 	s.AddAlternative("text/html", data.Html)
+
+// 	t := gomail.NewDialer("smtp.gmail.com", 465, user, pass)
+
+// 	if err := t.DialAndSend(s); err != nil {
+// 		return err // ❗ yahin galti thi
+// 	}
+
+// 	fmt.Println("email sent✅")
+// 	return nil
+// }
+
+package utils
+
+import (
+	"log"
+
+	"github.com/AbdulRahman-04/FullStackWebDev_2026/08_Go_Gin_PostgreSQL/server/internal/config"
+	"gopkg.in/gomail.v2"
+)
+
+type EmailData struct {
+	From string
+	To string
+	Subject string
+	Text string
+	Html string
+}
+
+func SendEmail(data EmailData) error {
+	// get user and pass 
+	user := config.AppConfig.Email
+	pass := config.AppConfig.Pass
+
+	// create sender
+	s := gomail.NewMessage()
+
+	s.SetAddressHeader("From", user, "Team social")
+	s.SetHeader("To", data.To)
+	s.SetHeader("Subject", data.Subject)
+	s.SetBody("text/plain", data.Text)
+	s.AddAlternative("text/html", data.Html)
+
+	// create transporter
+	t := gomail.NewDialer("smtp.gmail.com", 465, user, pass)
+
+	// try sending
+	if err := t.DialAndSend(s); err != nil {
+		return  err
+	}
+
+	log.Printf("email sent✅")
+	return  nil
+}
